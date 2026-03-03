00f4983f160b1266c7015fdf928719630582a8e2
Jacek Pasierb <jack_byk@o2.pl>
2026-03-03 19:02:17 +0100

    feat(client): scaffold react app with vite

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import {createServer} from "http";
+import {Server} from "socket.io";
+
 //initialize express app
 const app = express();
 const PORT = 4000;
+
+
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +20,23 @@ app.get("/health", (_req, res) => {
   res.json({ok: true});
 }); 
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: ["http://localhost:5173"],
+        methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("✅ WS connected:", socket.id);
+
+  socket.on("disconnect", (reason) => {
+    console.log("❌ WS disconnected:", socket.id, "reason:", reason);
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
